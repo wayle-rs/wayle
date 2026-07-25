DigitalPals/Lumen

fix(settings): open on the active workspace instead of first workspace
(cherry picked from commit e7bca6e0d9841cd8864812c90beb769749ca222f)

crates/lumen-settings/src/main.rs
@@ -15,6 +15,7 @@ mod sidebar;
 use std::{env, process};
 
 use lumen_config::{ConfigService, PersistenceWatcher};
+use relm4::gtk::prelude::*;
 use tokio::runtime::Runtime;
 use tracing_subscriber::EnvFilter;
 
@@ -72,7 +73,18 @@ fn main() {
         }
     };
 
-    let relm_app = relm4::RelmApp::new("com.lumen.settings");
+    let app = relm4::main_application();
+    app.set_application_id(Some("com.lumen.settings"));
+
+    // Raise the existing window when launched again; relm4 only calls
+    // set_visible, which is a no-op once the window is already shown.
+    app.connect_activate(|app| {
+        if let Some(window) = app.active_window() {
+            window.present();
+        }
+    });
+
+    let relm_app = relm4::RelmApp::from_app(app);
 
     relm_app.run::<app::SettingsApp>(config_service);
 }

crates/lumen-shell/src/shell/bar/dropdowns/dashboard/mod.rs
@@ -9,15 +9,12 @@ mod system_stats;
 mod user_session;
 mod watchers;
 
-use std::{
-    process::{Command, Stdio},
-    thread,
-};
-
 use gtk::prelude::*;
 use lumen_widgets::prelude::*;
-use relm4::{gtk, prelude::*};
-
+use relm4::{
+    gtk::{self, gdk, gio},
+    prelude::*,
+};
 pub(super) use self::factory::Factory;
 use self::{
     battery_section::{BatterySection, BatterySectionInit},
@@ -33,24 +30,35 @@ use crate::{i18n::t, shell::bar::dropdowns::scaled_dimension};
 
 const BASE_WIDTH: f32 = 380.0;
 
+/// Launches the settings app on the active workspace.
+///
+/// Launching through GDK's [`AppLaunchContext`](gdk::AppLaunchContext) gives the
+/// child a fresh `XDG_ACTIVATION_TOKEN` instead of the stale startup id the shell
+/// inherited at login, which would otherwise pin the window to workspace 1.
 fn spawn_settings_app() {
-    match Command::new("lumen-settings")
-        .stdin(Stdio::null())
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .spawn()
-    {
-        Ok(mut child) => {
-            thread::spawn(move || {
-                let _ = child.wait();
-            });
-        }
+    let Some(display) = gdk::Display::default() else {
+        tracing::warn!("no default display; cannot launch lumen-settings");
+        return;
+    };
+
+    let context = display.app_launch_context();
+
+    let app_info = match gio::AppInfo::create_from_commandline(
+        "lumen-settings",
+        Some("Lumen Settings"),
+        gio::AppInfoCreateFlags::NONE,
+    ) {
+        Ok(info) => info,
         Err(err) => {
-            tracing::warn!(error = %err, "failed to spawn lumen-settings");
+            tracing::warn!(error = %err, "failed to build lumen-settings launcher");
+            return;
         }
+    };
+
+    if let Err(err) = app_info.launch(&[], Some(&context)) {
+        tracing::warn!(error = %err, "failed to launch lumen-settings");
     }
 }
-
 pub(crate) struct DashboardDropdown {
     scaled_width: i32,
 
@@ -262,8 +270,9 @@ impl Component for DashboardDropdown {
                 self.media.emit(MediaSectionInput::SetActive(visible));
             }
             DashboardDropdownMsg::OpenSettings => {
-                root.popdown();
+                // Launch before popdown so the activation token anchors to the focused surface.
                 spawn_settings_app();
+                root.popdown();
             }
         }
     }

wayle-rs/wayle

crates/wayle-settings/src/main.rs
@@ -14,6 +14,7 @@ mod sidebar;
 
 use std::{env, process};
 
+use relm4::gtk::prelude::*;
 use tokio::runtime::Runtime;
 use tracing_subscriber::EnvFilter;
 use wayle_config::{ConfigService, PersistenceWatcher};
@@ -72,7 +73,18 @@ fn main() {
         }
     };
 
-    let relm_app = relm4::RelmApp::new("com.wayle.settings");
+    let app = relm4::main_application();
+    app.set_application_id(Some("com.wayle.settings"));
+
+    // Raise the existing window when launched again; relm4 only calls
+    // set_visible, which is a no-op once the window is already shown.
+    app.connect_activate(|app| {
+        if let Some(window) = app.active_window() {
+            window.present();
+        }
+    });
+
+    let relm_app = relm4::RelmApp::from_app(app);
 
     relm_app.run::<app::SettingsApp>(config_service);
 }

crates/wayle-shell/src/shell/bar/dropdowns/dashboard/mod.rs
@@ -9,13 +9,11 @@ mod system_stats;
 mod user_session;
 mod watchers;
 
-use std::{
-    process::{Command, Stdio},
-    thread,
-};
-
 use gtk::prelude::*;
-use relm4::{gtk, prelude::*};
+use relm4::{
+    gtk::{self, gdk, gio},
+    prelude::*,
+};
 use wayle_widgets::prelude::*;
 
 pub(super) use self::factory::Factory;
@@ -33,21 +31,33 @@ use crate::{i18n::t, shell::bar::dropdowns::scaled_dimension};
 
 const BASE_WIDTH: f32 = 380.0;
 
+/// Launches the settings app on the active workspace.
+///
+/// Launching through GDK's [`AppLaunchContext`](gdk::AppLaunchContext) gives the
+/// child a fresh `XDG_ACTIVATION_TOKEN` instead of the stale startup id the shell
+/// inherited at login, which would otherwise pin the window to workspace 1.
 fn spawn_settings_app() {
-    match Command::new("wayle-settings")
-        .stdin(Stdio::null())
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .spawn()
-    {
-        Ok(mut child) => {
-            thread::spawn(move || {
-                let _ = child.wait();
-            });
-        }
+    let Some(display) = gdk::Display::default() else {
+        tracing::warn!("no default display; cannot launch wayle-settings");
+        return;
+    };
+
+    let context = display.app_launch_context();
+
+    let app_info = match gio::AppInfo::create_from_commandline(
+        "wayle-settings",
+        Some("Wayle Settings"),
+        gio::AppInfoCreateFlags::NONE,
+    ) {
+        Ok(info) => info,
         Err(err) => {
-            tracing::warn!(error = %err, "failed to spawn wayle-settings");
+            tracing::warn!(error = %err, "failed to build wayle-settings launcher");
+            return;
         }
+    };
+
+    if let Err(err) = app_info.launch(&[], Some(&context)) {
+        tracing::warn!(error = %err, "failed to launch wayle-settings");
     }
 }
 
@@ -262,8 +272,9 @@ impl Component for DashboardDropdown {
                 self.media.emit(MediaSectionInput::SetActive(visible));
             }
             DashboardDropdownMsg::OpenSettings => {
-                root.popdown();
+                // Launch before popdown so the activation token anchors to the focused surface.
                 spawn_settings_app();
+                root.popdown();
             }
         }
     }